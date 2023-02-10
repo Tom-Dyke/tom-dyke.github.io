00c7ad1fdf2d712682fc68e6f7c06bce6a44d677
Thomas Dyke <118379392+Tom-Dyke@users.noreply.github.com>
2023-02-10 23:55:49 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+<html>
+    <head>
+        <script> 
+    		document.addEventListener("DOMContentLoaded", function() {
+  
 var buttonNames = ['Best', 'Very good', 'Good', 'Not good or bad', 'Not very good', 'Worst'];
 var buttons = [];
 for (var i = 0; i < buttonNames.length; i++) {
@@ -7,12 +12,16 @@ for (var i = 0; i < buttonNames.length; i++) {
   document.body.appendChild(button);
 }
 
-/ Highlight when i press one of the buttons */
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = function() {
-    for (var j = 0; j < buttons.length; j++) {
-      buttons[j].style.backgroundColor = 'white';
-    }
-    this.style.backgroundColor = 'yellow';
-  };
+	for (var i = 0; i < buttons.length; i++) {
+ 	 buttons[i].onclick = function() {
+ 	   for (var j = 0; j < buttons.length; j++) {
+ 	     buttons[j].style.backgroundColor = 'white';
+    	}
+	    this.style.backgroundColor = 'yellow';
+	  };
+	}
 }
+)
+		</script>
+    </head>
+</html>
