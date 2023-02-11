43f601521146bb716f3b8da46034fa0aeee661b0
Thomas Dyke <118379392+Tom-Dyke@users.noreply.github.com>
2023-02-11 00:00:27 +0000

    test

--- code.js
+++ code.js
@@ -3,25 +3,25 @@
         <script> 
     		document.addEventListener("DOMContentLoaded", function() {
   
-var buttonNames = ['Best', 'Very good', 'Good', 'Not good or bad', 'Not very good', 'Worst'];
-var buttons = [];
-for (var i = 0; i < buttonNames.length; i++) {
-  var button = document.createElement('button');
-  button.innerHTML = buttonNames[i];
-  buttons.push(button);
-  document.body.appendChild(button);
-}
+			var buttonNames = ['Best', 'Very good', 'Good', 'Not good or bad', 'Not very good', 'Worst'];
+			var buttons = [];
+			for (var i = 0; i < buttonNames.length; i++) {
+ 			 var button = document.createElement('button');
+ 			 button.innerHTML = buttonNames[i];
+  		 	 buttons.push(button);
+  			 document.body.appendChild(button);
+			 }
 
-	for (var i = 0; i < buttons.length; i++) {
- 	 buttons[i].onclick = function() {
- 	   for (var j = 0; j < buttons.length; j++) {
- 	     buttons[j].style.backgroundColor = 'white';
-    	}
-	    this.style.backgroundColor = 'yellow';
-	  };
-	}
-}
-)
+				for (var i = 0; i < buttons.length; i++) {
+ 				 buttons[i].onclick = function() {
+ 				   for (var j = 0; j < buttons.length; j++) {
+ 				     buttons[j].style.backgroundColor = 'white';
+   			 	}
+	 			   this.style.backgroundColor = 'yellow';
+				  };
+				}
+			}
+		)
 		</script>
     </head>
 </html>
